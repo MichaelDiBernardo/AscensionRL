4ffc99c71f64ad6e738d96753ace334c51c62c7d
Michael DiBernardo <mikedebo@gmail.com>
2013-11-16 15:43:24 -0500

    Rejigging test a bit.

--- assets/equipment_test.js
+++ assets/equipment_test.js
@@ -1,33 +1,36 @@
 (function() {
-    var fakeRepo = new Game.EntityRepository();
-    fakeRepo.define("sword", {
-        name: "Sword",
-        damageDice: 1,
-        damageSides: 7,
-        meleeBonus: 0,
-        evasionBonus: 1,
-        weight: 10,
-        slotType: "SLOT_WEAPON",
-        mixins: [Game.Mixins.Item, Game.Mixins.Wearable, Game.Mixins.Weapon]
-    });
+    var fakeRepo, sut;
 
-    fakeRepo.define("armor", {
-        name: "Armor",
-        weight: 100,
-        slotType: "SLOT_BODY",
-        mixins: [Game.Mixins.Item, Game.Mixins.Wearable, Game.Mixins.Armor]
-    });
+    module("equipment", {
+        setup: function() {
+            fakeRepo = new Game.EntityRepository();
+            fakeRepo.define("sword", {
+                name: "Sword",
+                damageDice: 1,
+                damageSides: 7,
+                meleeBonus: 0,
+                evasionBonus: 1,
+                weight: 10,
+                slotType: "SLOT_WEAPON",
+                mixins: [Game.Mixins.Item, Game.Mixins.Wearable, Game.Mixins.Weapon]
+            });
 
-    module("equipment");
+            fakeRepo.define("armor", {
+                name: "Armor",
+                weight: 100,
+                slotType: "SLOT_BODY",
+                mixins: [Game.Mixins.Item, Game.Mixins.Wearable, Game.Mixins.Armor]
+            });
+            sut = new Game.Equipment();
+        }
+    });
 
     test("Test empty equipment protection.", function() {
-        var sut = new Game.Equipment();
         deepEqual(sut.protection(), [0, 0]);
     });
 
     test("Test empty equipment has none in all slots.", function() {
-        var sut = new Game.Equipment(),
-            slotTypes = sut.getSlotTypes(),
+        var slotTypes = sut.getSlotTypes(),
             length = slotTypes.length;
 
         for (var i = 0; i< length; i++) {
@@ -37,7 +40,6 @@
     });
 
     test("Test equip weapon into empty slot.", function() {
-        var sut = new Game.Equipment();
         sut.equip(fakeRepo.create("sword"));
 
         var weapon = sut.getWearableInSlot("SLOT_WEAPON");
@@ -46,7 +48,6 @@
     });
 
     test("Test equip armor into empty slot.", function() {
-        var sut = new Game.Equipment();
         sut.equip(fakeRepo.create("armor"));
 
         var armor = sut.getWearableInSlot("SLOT_BODY");
@@ -54,9 +55,6 @@
     });
 
     test("Test equip something into bad slot.", function() {
-        var sut = new Game.Equipment(),
-            passed = false;
-
         fakeRepo.define("weirdthing", {
             name: "WhichSlot",
             weight: 100,
@@ -71,4 +69,5 @@
             "No exception raised for equipping to bad slot."
         );
     });
+
 })();
