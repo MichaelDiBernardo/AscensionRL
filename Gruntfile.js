55436870d727da5bf8b63e1b9969e7bcae2cb017
Michael DiBernardo <mikedebo@gmail.com>
2013-11-30 22:25:10 -0500

    Fun with gruntfiles.

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,17 +38,37 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint', 'qunit']
+    },
+    copy: {
+      main: {
+        src: 'index.html',
+        // JUST FOR NOW UNTIL WE GET MINIFY WORKING
+        dest: 'index-live.html'
+      },
+    },
+    asset_cachebuster: {
+      options: {
+        buster: Date.now(),
+        htmlExtension: 'html'
+      },
+      default: {
+        files: {
+          'index-live.html': ['index.html']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-asset-cachebuster');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
 
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify', 'copy', 'asset_cachebuster']);
 
 };
